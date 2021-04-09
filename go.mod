3852b265a3f97a20658237bff186c4a50801e5a5
Ze Xuan <syltaris@gmail.com>
2021-04-09 12:59:46 +0800

    sentiment part seems to work nicely

--- go.mod
+++ go.mod
@@ -4,6 +4,15 @@ go 1.16
 
 require (
 	github.com/bwmarrin/discordgo v0.23.2 // indirect
+	github.com/cdipaolo/goml v0.0.0-20190412180403-e1f51f713598 // indirect
+	github.com/cdipaolo/sentiment v0.0.0-20200617002423-c697f64e7f10 // indirect
+	github.com/go-audio/audio v1.0.0 // indirect
+	github.com/go-audio/wav v1.0.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/pion/rtp v1.6.2 // indirect
 	github.com/pion/webrtc/v3 v3.0.20 // indirect
+	github.com/sethgrid/gencurl v0.0.0-20161025011400-a3af93c1aba4 // indirect
+	github.com/wit-ai/wit-go v1.0.13 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	gopkg.in/hraban/opus.v2 v2.0.0-20201025103112-d779bb1cc5a2 // indirect
 )
--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
 	"sync"
 	"time"
 
+	"github.com/joho/godotenv"
+
 	"github.com/bwmarrin/discordgo"
+	"github.com/cdipaolo/sentiment"
 	"github.com/pion/rtp"
 	"github.com/pion/webrtc/v3/pkg/media"
 	"github.com/pion/webrtc/v3/pkg/media/oggwriter"
+	witai "github.com/wit-ai/wit-go"
 )
 
-// TODO: regen this and put in env before deploy!
-var botToken = "ODI5NTk4MDcxMjAxMjAyMjI3.YG6daQ.bpvwnty2nkoWCZVCD1mctWZ3Gyc"
+var witAiClient *witai.Client
+
 var guildId = "829599334127501312"
 var channelId = "829599334127501316"
 
@@ -39,7 +45,7 @@ func handleVoice(v *discordgo.VoiceConnection, wg *sync.WaitGroup) {
 		file, ok := files[p.SSRC]
 		if !ok {
 			var err error
-			file, err = oggwriter.New(fmt.Sprintf("%d.ogg", p.SSRC), 48000, 2)
+			file, err = oggwriter.New(fmt.Sprintf("%d.ogg", p.SSRC), 48000, 1)
 			if err != nil {
 				fmt.Printf("failed to create file %d.ogg, giving up on recording: %v\n", p.SSRC, err)
 				return
@@ -48,30 +54,75 @@ func handleVoice(v *discordgo.VoiceConnection, wg *sync.WaitGroup) {
 		}
 		// Construct pion RTP packet from DiscordGo's type.
 		rtp := createPionRTPPacket(p)
+		
 		err := file.WriteRTP(rtp)
 		if err != nil {
 			fmt.Printf("failed to write to file %d.ogg, giving up on recording: %v\n", p.SSRC, err)
 		}
 
-		v.OpusSend <- rtp.Payload
 	}
-
 	// Once we made it here, we're done listening for packets. Close all files
 	for _, f := range files {
 		f.Close()
 	}	
+	
+	//
+	file, err := os.Open("593398.ogg")
+	if err != nil {
+		fmt.Println("can't open file:" ,err)
+	}
+	// send to wit AI to parse
+	speech := witai.Speech{File: file, ContentType: "audio/ogg"}
+	msg, err := witAiClient.Speech(&witai.MessageRequest{Speech: &speech})
+	if err != nil {
+		fmt.Println("can't send to witAi:", err)
+	}
+	fmt.Println("output: ",msg.Text, msg)
+
+	// temp debug code
+	rand.Seed(time.Now().Unix())
+	choices := []string{
+		"get rekt noob",
+		"wow yes i did it",
+		"totally nice job",
+		"wow you suck eggs",
+		"eat my shorts",
+	}
+	outputText := choices[rand.Int() % len(choices)]
+
+	// suppose we got the output here, feed it to sentiment engine and see result
+	model, err := sentiment.Restore() 
+		if err != nil {  
+		panic(err) 
+	} 
+	var analysis *sentiment.Analysis
+	analysis = model.SentimentAnalysis(outputText, sentiment.English)
+	fmt.Println("score:", analysis.Score, outputText)
+	if analysis.Score == 1{
+		// play congrats sound
+		
+	} else {
+		// play oh noes sound
+	}
+
 	wg.Done()
 }
 
 func main() {
-	s, err := discordgo.New("Bot "+ botToken);
+	err := godotenv.Load(".env")
+	botToken := os.Getenv("DISCORD_BOT_TOKEN")
+	witAiToken := os.Getenv("WIT_AI_TOKEN")
 
+	s, err := discordgo.New("Bot "+ botToken);
 	if err != nil {
 		fmt.Println("can't init Discord sesh:", err)
 		return
 	}
-
 	defer s.Close()
+
+	// init witAi client
+	witAiClient = witai.NewClient(witAiToken)
+
 
 	// configure listener's tracked intents?
 	s.Identify.Intents = discordgo.IntentsGuildVoiceStates
@@ -91,10 +142,11 @@ func main() {
 	wg := sync.WaitGroup{}
 
 	wg.Add(1)
-	handleVoice(v, &wg)
 	go func() {
 		time.Sleep(3 * time.Second)
+		close(v.OpusRecv)
 		v.Close()
 	}()
+	handleVoice(v, &wg)
 	wg.Wait()
 }
